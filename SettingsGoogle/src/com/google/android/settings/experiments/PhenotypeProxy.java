c30b646488db412927d962439a383604a8cd14d7
Alvin Francis <nivlafx@gmail.com>
2023-04-29 14:44:52 -0400

    Updates to phenotypeproxy
    Change-Id: I69ab984d1bc845f661330ce377b3d6615de63288

--- SettingsGoogle/src/com/google/android/settings/experiments/PhenotypeProxy.java
+++ SettingsGoogle/src/com/google/android/settings/experiments/PhenotypeProxy.java
@@ -10,47 +10,65 @@ public class PhenotypeProxy {
     private static final Uri PROXY_AUTHORITY = new Uri.Builder().scheme("content").authority("com.google.android.settings.intelligence.provider.experimentflags").build();
 
     public static boolean getFlagByPackageAndKey(Context context, String str, String str2, boolean z) {
+        ContentProviderClient acquireUnstableContentProviderClient;
         Bundle bundle = new Bundle();
         bundle.putString("package_name", str);
         bundle.putString("key", str2);
         Bundle bundle2 = null;
         try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
+            acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
             bundle2 = acquireUnstableContentProviderClient.call("getBooleanForPackageAndKey", null, bundle);
             acquireUnstableContentProviderClient.close();
         } catch (Exception e) {
             Log.e("PhenotypeProxy", "Failed to query experiment provider", e);
+        } catch (Throwable th) {
+            th.addSuppressed(th);
         }
-        return bundle2 == null ? z : bundle2.getBoolean("value", z);
+        if (bundle2 == null) {
+            return z;
+        }
+        return bundle2.getBoolean("value", z);
     }
 
     public static boolean getBooleanFlagByPackageAndKey(Context context, String str, String str2, boolean b) {
+        ContentProviderClient acquireUnstableContentProviderClient;
         Bundle bundle = new Bundle();
         bundle.putString("package_name", str);
         bundle.putString("key", str2);
         Bundle bundle2 = null;
         try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
+            acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
             bundle2 = acquireUnstableContentProviderClient.call("getBooleanForPackageAndKey", null, bundle);
             acquireUnstableContentProviderClient.close();
         } catch (Exception e) {
             Log.e("PhenotypeProxy", "Failed to query experiment provider", e);
+        } catch (Throwable th) {
+            th.addSuppressed(th);
         }
-        return bundle2 == null ? b : bundle2.getBoolean("value", b);
+        if (bundle2 == null) {
+            return b;
+        }
+        return bundle2.getBoolean("value", b);
     }
 
     public static String getStringFlagByPackageAndKey(Context context, String str, String str2, String s2) {
+        ContentProviderClient acquireUnstableContentProviderClient;
         Bundle bundle = new Bundle();
         bundle.putString("package_name", str);
         bundle.putString("key", str2);
         Bundle bundle2 = null;
         try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
+            acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
             bundle2 = acquireUnstableContentProviderClient.call("getStringForPackageAndKey", null, bundle);
             acquireUnstableContentProviderClient.close();
         } catch (Exception e) {
             Log.e("PhenotypeProxy", "Failed to query experiment provider", e);
+        } catch (Throwable th) {
+            th.addSuppressed(th);
         }
-        return bundle2 == null ? s2 : bundle2.getString("value", s2);
+        if (bundle2 == null) {
+            return s2;
+        }
+        return bundle2.getString("value", s2);
     }
 }
